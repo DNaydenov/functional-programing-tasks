e7035c78f1cc5b0f4cd3be1dbbac07f0f0fe65f7
DNaydenov <dimitar.naidenov.98@gmail.com>
2019-11-10 20:15:24 +0200

    final update i hope

--- 00/solutions/square-root.rkt
+++ 00/solutions/square-root.rkt
@@ -2,12 +2,10 @@
 
 (provide my-sqrt)
 
-
 (define (my-sqrt x)
   (define (call-me last-res last-last)
-    (if (< (- last-res last-last) 0.0001)
+    (if (<= (abs (- last-res last-last)) 0.00001)
         last-res
-        (call-me (exact->inexact
-                  (/ (+ last-res (exact->inexact (/ x last-res))) 2))
+        (call-me (/ (+ last-res (/ x last-res)) 2.0)
                  last-res)))
   (call-me 10 0))
