aa622ea08527f5bd252526aeae9c7ee4f29e7ef3
DNaydenov <dimitar.naidenov.98@gmail.com>
2019-11-10 18:17:06 +0200

    latest update

--- 00/solutions/k-ary.rkt
+++ 00/solutions/k-ary.rkt
@@ -3,6 +3,25 @@
 (provide from-k-ary
          to-k-ary)
 
-(define (from-k-ary n k) void)
+(define (from-k-ary n k) 
+  (define (helper acc counter rest)
+    (if (zero? rest)
+        acc
+        (helper (+ acc
+                   (* (remainder rest 10)
+                      (expt k counter))) (+ 1 counter)
+                                         (quotient  rest 10))))
+  (helper 0 0 n))	
 
-(define (to-k-ary n k) void)
+
+
+(define (to-k-ary n k)
+  (define (helper acc counter rest)
+    (if (zero? rest)
+        acc
+        (helper (+ (* (remainder rest k) (expt 10 counter))
+                   acc)
+                (+ 1 counter)
+                (quotient rest k))))
+  (helper 0 0 n))
+
--- 00/solutions/square-root.rkt
+++ 00/solutions/square-root.rkt
@@ -2,4 +2,16 @@
 
 (provide my-sqrt)
 
-(define (my-sqrt x) void)
+(define (round-off z n)
+  (let ((power (expt 10 n)))
+    (/ (round (* power z)) power)))
+
+(define (my-sqrt x)
+  (define (call-me last-res goal-res)
+    (if (= (round-off last-res 4) (round-off goal-res 4))
+        (round-off last-res 4)
+        (call-me (exact->inexact
+                  (/ (+ last-res (exact->inexact (/ x last-res))) 2))
+                 goal-res)))
+  (call-me 10 (sqrt x)))
+
