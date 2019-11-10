972f0a71ba615ed2e18101cde0c010f15c40744e
DNaydenov <dimitar.naidenov.98@gmail.com>
2019-11-10 19:32:44 +0200

    update squere

--- 00/solutions/square-root.rkt
+++ 00/solutions/square-root.rkt
@@ -2,14 +2,10 @@
 
 (provide my-sqrt)
 
-(define (round-off z n)
-  (let ((power (expt 10 n)))
-    (/ (round (* power z)) power)))
-
 (define (my-sqrt x)
   (define (call-me last-res last-last)
     (if (< (- last-res last-last) 0.0001)
-        (round-off last-res 4)
+        last-res
         (call-me (exact->inexact
                   (/ (+ last-res (exact->inexact (/ x last-res))) 2))
                  last-res)))
