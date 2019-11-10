8f52e3a1f6f11158f39b588f8ab6004336d43858
DNaydenov <dimitar.naidenov.98@gmail.com>
2019-11-10 20:10:57 +0200

    repair error on my-sqrt

--- 00/solutions/square-root.rkt
+++ 00/solutions/square-root.rkt
@@ -2,6 +2,7 @@
 
 (provide my-sqrt)
 
+
 (define (my-sqrt x)
   (define (call-me last-res last-last)
     (if (< (- last-res last-last) 0.0001)
@@ -10,4 +11,3 @@
                   (/ (+ last-res (exact->inexact (/ x last-res))) 2))
                  last-res)))
   (call-me 10 0))
-
