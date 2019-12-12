85481215f6966794e148f5faa998874e002c8717
DNaydenov <dimitar.naidenov.98@gmail.com>
2019-12-12 19:46:28 +0200

    predavam na chisto matrix.rkt, xno.rkt

--- 01/solutions/matrix.rkt
+++ 01/solutions/matrix.rkt
@@ -16,44 +16,107 @@
          zip-matrix)
 ; the provide "exports" these functions
 
-; 00.
-(define (all? p? xs) void)
+; 00. re
+(define (all? p? xs)
+ (foldr (lambda (xs rec)
+          (and (p? xs)
+               rec))
+        #t xs))
 
-; 01.
-(define (any? p? xs) void)
 
-; 02.
-(define (concat xss) void)
+; 01. re
+(define (any? p? xs)
+  (foldr (lambda (xs rec)
+          (or (p? xs)
+               rec))
+        #f xs))
 
-; 03.
-(define (rows xss) void)
+  
 
-; 04.
-(define (cols xss) void)
+; 02. re
+(define (concat xss)
+  (foldr (lambda (xs rec)
+           (append xs rec))
+         `() xss ))
+
+; 03. re
+(define (rows xss) xss)
+
+; 04. ? to do 
+(define (cols xss)
+  (if (null? (car xss)) `()
+      (cons (map car xss)
+            (cols (map cdr xss)))))
 
 ; 05.
-(define (matrix-ref xss i j) void)
+(define (matrix-ref xss i j)
+  (cond ((null? xss) -1)
+        ((zero? i) (list-ref (car xss) j))
+        (else (matrix-ref (cdr xss) (- i 1) j))))
 
 ; 06.
-(define (set xs i x) void)
+(define (set xs i x)
+  (cond ((null? xs) `())
+        ((zero? i) (cons x (cdr xs)))
+        (else (cons (car xs) (set (cdr xs) (- i 1) x)))))
 
 ; 07.
-(define (place xss i j x) void)
+(define (place xss i j x)
+  (cond ((null? xss) `())
+        ((zero? i) (cons (set (car xss) j x)
+                         (cdr xss)))
+        (else (cons (car xss)
+                    (place (cdr xss) (- i 1) j x)))))
 
 ; 08.
-(define (diag xss) void)
+(define (diag xss)
+  (if (null? xss) `()
+      (cons (car (car xss))
+            (diag (map cdr (cdr xss))))))
 
 ; 09.
-(define (diags xss) void)
+(define (diags xss)
+  (define (reverse-matrix xss)
+    (if (null? xss) `()
+        (append (reverse-matrix (cdr xss)) (list (car xss)))))
+  (list (diag xss) (reverse (diag (reverse-matrix xss)))))
 
 ; 10.
-(define (map-matrix f xss) void)
+(define (map-matrix f xss)
+  (map (lambda (xs) (map f xs)) xss))
+
+;(define (1+ x) (+ 1 x))
+(define (id x) x)
+(define (const x) (lambda (y) x))
+(map-matrix id         '((1337)))  ;-- '((1337))
+;(map-matrix 1+         '((1 0)
+ ;                       (0 1)))   ;-- '((2 1)
+                                   ;     (1 2))
+(map-matrix (const 69) '((1 2 3)
+                         (4 5 6)
+                         (7 8 9))) ;-- '((69 69 69)
+                                   ;     (69 69 69)
+                                   ;     (69 69 69))
+
 
 ; 11.
-(define (filter-matrix p? xss) void)
+(define (filter-matrix p? xss)
+  (if (null? xss) `()
+      (cons (filter p? (car xss))
+            (filter-matrix p? (cdr xss)))))
 
 ; 12.
-(define (zip-with f xs ys) void)
+(define (zip-with f xs ys)
+  (map f
+       (take xs (min (length xs)
+                     (length ys)))
+       (take ys (min (length xs)
+                     (length ys)))))
 
 ; 13.
-(define (zip-matrix xss yss) void)
+(define (zip-matrix xss yss)
+  (if (or (null? xss)
+           (null? yss))
+      `()
+      (cons (zip-with cons (car xss) (car yss))
+            (zip-matrix (cdr xss) (cdr yss)))))
--- 01/solutions/xno.rkt
+++ 01/solutions/xno.rkt
@@ -11,16 +11,50 @@
 ; winner implementation that only detects draws right now.
 ; Put your own implementation here!
 (define (winner b)
-  (if (andmap (lambda (xs) (andmap id xs)) b)
-      "D"
-      #f))
+  (define (winner-x? symbol)
+     (ormap (lambda (xs) (all? (lambda (x) (eqv? x symbol)) xs))
+            (append (rows b) (cols b) (diags b))))
+        
+  (cond ((winner-x? "X") "X")
+        ((winner-x? "O") "O")
+        ((andmap (lambda (xs) (andmap id xs)) b) "D")       
+        (else #f)))
 
+(define (possible-turns b)
+  (define (iter i j)
+    (cond ((= i (length b)) `())
+          ((= j (length b)) (iter (+ i 1) 0))
+          ((eqv? (matrix-ref b i j ) #f) (cons (cons i j) (iter i (+ j 1))))
+          (else (iter i (+ j 1)))))
+    (iter 0 0))
 ; "Dumb" "AI", plays the "next" free spot, going left-to-right, top-to-bottom.
 ; Put your own implementation here!
+
+
+
+(define (best-outcome game p maximilize? perspective)
+  (define minimax (if (eqv? maximilize? #t) max min))
+  (define not-p (if (eqv? p "O") "X" "O"))
+  (define winner. (winner game))
+  (define possible-turns. (possible-turns game))
+  
+  (cond ((eqv? winner. perspective) 1)
+        ((eqv? winner. "D") 0)
+        ((eqv? winner. (if (eqv? perspective "O") "X" "O")) -1)
+        (else (foldr  minimax (if maximilize? -200 200)
+                      (map (lambda (ij)
+                             (best-outcome (place game (car ij) (cdr ij) p) not-p (not maximilize?) perspective))
+                           possible-turns.))))) 
+
+
+ 
+(define (play-1 curr-board curr-sign)
+  (define not-p (if (eqv? curr-sign "O") "X" "O"))
+  (car (foldl (lambda (pair rec) (if (> (cdr pair) (cdr rec)) pair rec))
+         (cons (cons 0 0) -2)
+         (map (lambda (turn) (cons turn
+                                   (best-outcome (place curr-board (car turn) (cdr turn) curr-sign) not-p #f curr-sign)))
+              (possible-turns curr-board)))))
+
 (define (play curr-board curr-sign)
-  (define (helper i j)
-    (cond ((> i 2) #f)
-          ((> j 2) (helper (+ i 1) 0))
-          ((not (list-ref (list-ref curr-board i) j)) (cons i j))
-          (else (helper i (+ j 1)))))
-  (helper 0 0))
+  (play-1 curr-board (if curr-sign "X" "O")))
